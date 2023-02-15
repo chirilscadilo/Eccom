9d72f06e8c7cf61766c69e687ed962891374df6f
chiri <chirilscadilo@gmail.com>
2023-02-15 12:30:53 +0200

    modified:   ecommerce/settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,3 +141,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #ALLOWED_HOSTS = ["web-production-d22f.up.railway.app","chrome-extension://ghbmnnjooekpmoecnnnilnnbdlolhkhi"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000'
+],
+ALLOWED_HOSTS = [
+    'localhost',
+],
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+]
